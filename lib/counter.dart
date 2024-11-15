d12e07304e2782a00a2bf3b42fd8bab12896f2c1
M Sibtain Zahid <msibtain1236@gmail.com>
2024-11-15 15:14:09 +0500

    counter Logic

--- lib/counter.dart
+++ lib/counter.dart
@@ -1,28 +1,57 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 
-class CounterScreen extends StatefulWidget {
-  const CounterScreen({super.key});
+void main() => runApp(MyApp());
 
+class MyApp extends StatelessWidget {
   @override
-  State<CounterScreen> createState() => _CounterScreenState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: CounterScreen(),
+    );
+  }
+}
+
+class CounterScreen extends StatefulWidget {
+  @override
+  _CounterScreenState createState() => _CounterScreenState();
 }
 
 class _CounterScreenState extends State<CounterScreen> {
-      int counter = 1;
+  int counter = 0;
+  String evenOdd = 'Even'; // Initial display message
+
+  void incrementCounter() {
+    setState(() {
+      counter++;
+      evenOdd = (counter % 2 == 0) ? 'Even' : 'odd' ;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text('counter'),),
-        body: Center(child: Text(counter.toString(),style: TextStyle(color: Colors.black,fontSize: 20,fontWeight: FontWeight.w600),)),
-        floatingActionButton: FloatingActionButton(onPressed: (){
-          setState(() {
-            counter++;
-            log(counter.toString());
-          });
-        }, child: Text('+'),),
+      appBar: AppBar(
+        title: Text('Counter App'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Counter: $counter',
+            style: TextStyle(fontSize: 25),),
+            SizedBox(height: 10,),
+            Text(' The number is $evenOdd',
+            style: TextStyle(fontSize: 25),),
+            
+
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: incrementCounter,
+        child: Icon(Icons.add),
+      ),
     );
   }
 }
